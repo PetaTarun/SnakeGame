daaa197df027ec001f977faded9dba1a1cb2ac91
tarun <tarun@DESKTOP-83VNE9M>
2024-03-25 10:48:27 +0530

    updated frames

--- SnakeGame/src/SnakeGame/SnakeGame.java
+++ SnakeGame/src/SnakeGame/SnakeGame.java
@@ -1,7 +1,14 @@
 package SnakeGame;
 
-public class SnakeGame {
+import javax.swing.JFrame;
 
+public class SnakeGame extends JFrame{
+	SnakeGame(){
+		setLocationRelativeTo(null);
+		setSize(300,400);
+		setVisible(true);
+	
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		new SnakeGame();
